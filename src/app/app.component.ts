7e131d548012571cce34714ff626f749bda963e0
ikhlaq <ikhlaq.firdous@blackstraw.ai>
2020-03-31 15:35:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,90 @@
 import { Component } from '@angular/core';
 
+const surveys = [{
+  id: 1,
+  questions: [
+    {
+      question: 'How old are you?',
+      type: 'text'
+    },
+    {
+    question: 'Please select your Gender?',
+    options: [
+      {value: 'male', label: 'Male'},
+      {value: 'female', label: 'Female'},
+      {value: 'other', label: 'Other'},
+    ],
+    type: 'radio',
+  },{
+    question: 'Please let us know your current body temperature in degree Fahrenheit (Normal body temperature is 98.6°F):',
+    options: [
+      {value: 'normal', label: 'Normal (96°F-98.6°F)'},
+      {value: 'fever', label: 'Fever (98.6°F-102°F)'},
+      {value: 'high-fever', label: 'High Fever > (102°F)'},
+    ],
+    type: 'radio',
+  },
+  {
+    question: 'Are you experiencing any of the symptoms below (mark all those applicable)',
+    options: [
+      {value: 'normal', label: 'Normal (96°F-98.6°F)'},
+      {value: 'fever', label: 'Fever (98.6°F-102°F)'},
+      {value: 'high-fever', label: 'High Fever > (102°F)'},
+    ],
+    type: 'checkbox',
+  },
+  {
+    question: 'Additionally, please verify if you are experiencing any of the symptoms below (mark all those applicable)',
+    options: [
+      {value: 'moderate-cough', label: 'Moderate to severe cough'},
+      {value: 'fever', label: 'Feeling breathless'},
+      {value: 'high-fever', label: 'High Fever > (102°F)'},
+    ],
+    type: 'checkbox',
+  },
+  {
+    question: 'Please select your travel and exposure details?',
+    options: [
+      {value: 'male', label: 'No contact with anyone with Symptoms'},
+      {value: 'female', label: 'Female'},
+      {value: 'other', label: 'Other'},
+    ],
+    type: 'radio',
+  },
+  {
+    question: 'Do you have a history of any of these conditions (mark all those applicable)',
+    options: [
+      {value: 'moderate-cough', label: 'Dibetes'},
+      {value: 'fever', label: 'Feeling breathless'},
+      {value: 'high-fever', label: 'High Fever > (102°F)'},
+    ],
+    type: 'checkbox',
+  },
+  {
+    question: 'How have your symptoms progressed over the last 48 hrs??',
+    options: [
+      {value: 'male', label: 'Improved'},
+      {value: 'female', label: 'Female'},
+      {value: 'other', label: 'Other'},
+    ],
+    type: 'radio',
+  },
+    , {
+    question: 'What did you learn from this course that you didn\'t know before?',
+    options: [
+      {value: "angular", label: "Angular"},
+      {value: "cli", label: "Angular CLI"},
+      {value: "pwa", label: "Progressive Web Apps"},
+      {value: "spa", label: "Single Page Application using Anuglar"},
+      {value: "ssr", lable: "Server Side Rendering using Angular Universal"},
+      {value: "monorepo", label: "Creating Mono Repo App using Nx"}
+    ],
+    type: 'select',
+  }, {
+    question: 'Whats your feedback about the course?',
+    type: 'text'
+  }]
+}]
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +92,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cornavirus-risk';
+  seasons: number[] = [2, 1];
+  questions = surveys[0].questions;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {QuestionsLoopDirective} from '../app/questions-loop.directive';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionsLoopDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    MatToolbarModule,
+    MatSelectModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
